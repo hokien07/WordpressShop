917203ec11550526b48edc27c3d92fb27cd404d8
hokien07 <hokien07@gmail.com>
2017-10-14 13:36:18 +0700

    them database

---
+++ database/banhang_db.sql
@@ -0,0 +1,236 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 14, 2017 at 08:32 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `banhang_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_commentmeta`
+--
+-- Error reading structure for table banhang_db.bh_commentmeta: #1932 - Table 'banhang_db.bh_commentmeta' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_commentmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_commentmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_comments`
+--
+-- Error reading structure for table banhang_db.bh_comments: #1932 - Table 'banhang_db.bh_comments' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_comments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_comments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_links`
+--
+-- Error reading structure for table banhang_db.bh_links: #1932 - Table 'banhang_db.bh_links' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_links: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_links`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_options`
+--
+-- Error reading structure for table banhang_db.bh_options: #1932 - Table 'banhang_db.bh_options' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_options: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_options`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_postmeta`
+--
+-- Error reading structure for table banhang_db.bh_postmeta: #1932 - Table 'banhang_db.bh_postmeta' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_postmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_postmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_posts`
+--
+-- Error reading structure for table banhang_db.bh_posts: #1932 - Table 'banhang_db.bh_posts' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_posts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_posts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_termmeta`
+--
+-- Error reading structure for table banhang_db.bh_termmeta: #1932 - Table 'banhang_db.bh_termmeta' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_termmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_termmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_terms`
+--
+-- Error reading structure for table banhang_db.bh_terms: #1932 - Table 'banhang_db.bh_terms' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_terms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_terms`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_term_relationships`
+--
+-- Error reading structure for table banhang_db.bh_term_relationships: #1932 - Table 'banhang_db.bh_term_relationships' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_term_relationships: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_term_relationships`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_term_taxonomy`
+--
+-- Error reading structure for table banhang_db.bh_term_taxonomy: #1932 - Table 'banhang_db.bh_term_taxonomy' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_term_taxonomy: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_term_taxonomy`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_usermeta`
+--
+-- Error reading structure for table banhang_db.bh_usermeta: #1932 - Table 'banhang_db.bh_usermeta' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_usermeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_usermeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_users`
+--
+-- Error reading structure for table banhang_db.bh_users: #1932 - Table 'banhang_db.bh_users' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_users`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_api_keys`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_api_keys: #1932 - Table 'banhang_db.bh_woocommerce_api_keys' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_api_keys: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_api_keys`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_attribute_taxonomies`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_attribute_taxonomies: #1932 - Table 'banhang_db.bh_woocommerce_attribute_taxonomies' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_attribute_taxonomies: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_attribute_taxonomies`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_downloadable_product_permissions`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_downloadable_product_permissions: #1932 - Table 'banhang_db.bh_woocommerce_downloadable_product_permissions' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_downloadable_product_permissions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_downloadable_product_permissions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_log`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_log: #1932 - Table 'banhang_db.bh_woocommerce_log' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_log: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_log`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_order_itemmeta`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_order_itemmeta: #1932 - Table 'banhang_db.bh_woocommerce_order_itemmeta' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_order_itemmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_order_itemmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_order_items`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_order_items: #1932 - Table 'banhang_db.bh_woocommerce_order_items' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_order_items: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_order_items`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_payment_tokenmeta`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_payment_tokenmeta: #1932 - Table 'banhang_db.bh_woocommerce_payment_tokenmeta' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_payment_tokenmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_payment_tokenmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_payment_tokens`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_payment_tokens: #1932 - Table 'banhang_db.bh_woocommerce_payment_tokens' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_payment_tokens: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_payment_tokens`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_sessions`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_sessions: #1932 - Table 'banhang_db.bh_woocommerce_sessions' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_sessions: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_sessions`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_shipping_zones`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_shipping_zones: #1932 - Table 'banhang_db.bh_woocommerce_shipping_zones' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_shipping_zones: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_shipping_zones`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_shipping_zone_locations`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_shipping_zone_locations: #1932 - Table 'banhang_db.bh_woocommerce_shipping_zone_locations' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_shipping_zone_locations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_shipping_zone_locations`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_shipping_zone_methods`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_shipping_zone_methods: #1932 - Table 'banhang_db.bh_woocommerce_shipping_zone_methods' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_shipping_zone_methods: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_shipping_zone_methods`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_tax_rates`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_tax_rates: #1932 - Table 'banhang_db.bh_woocommerce_tax_rates' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_tax_rates: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_tax_rates`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bh_woocommerce_tax_rate_locations`
+--
+-- Error reading structure for table banhang_db.bh_woocommerce_tax_rate_locations: #1932 - Table 'banhang_db.bh_woocommerce_tax_rate_locations' doesn't exist in engine
+-- Error reading data for table banhang_db.bh_woocommerce_tax_rate_locations: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `banhang_db`.`bh_woocommerce_tax_rate_locations`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
